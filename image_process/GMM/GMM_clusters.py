8577f42c3e84281951ce40a578753244dd5681ee
LQXshane <qxlin@bu.edu>
2016-11-10 17:54:05 -0500

    add visuals and init distance cal

---
+++ image_process/GMM/GMM_clusters.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+# __author__ = qiuxuan.lin
+
+
+import cv2
+import numpy as np
+from sklearn import mixture
+from IPython import embed
+# import matplotlib.pyplot as plt
+
+
+img = cv2.imread('simple.jpg')
+
+g = mixture.GMM(n_components=8)
+Z = img.reshape((-1,3))
+Z = np.float32(Z)
+
+g.fit(Z)
+
+label = g.predict(Z)
+center = g.means_ # centroid RGB values
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape((img.shape))
+
+
+
+cv2.imshow('img', img)
+cv2.imshow('gmmed', res2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
--- image_process/kmeans/color_quantization.py
+++ image_process/kmeans/color_quantization.py
@@ -2,13 +2,13 @@ import numpy as np
 import cv2
 from IPython import embed
 
-img = cv2.imread('mexico.jpg')
+img = cv2.imread('simple.jpg')
 Z = img.reshape((-1,3))
 Z = np.float32(Z)
 # embed()
 # define criteria, number of clusters(K) and apply kmeans()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 2
+K = 8
 ret,label,center=cv2.kmeans(Z,K,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 # embed()
 # Now convert back into uint8, and m
@@ -16,7 +16,7 @@ ret,label,center=cv2.kmeans(Z,K,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 center = np.uint8(center)
 res = center[label.flatten()]
 res2 = res.reshape((img.shape))
-embed()
+# embed()
 cv2.imshow('res2',res2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
